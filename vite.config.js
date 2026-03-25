1f0649d1a5d8a12133604959049e0ee8fdc66403
advaitparnaik <advait@hellomirari.com>
2026-03-25 20:18:25 +0530

    fix: vite config icons indentation

--- vite.config.js
+++ vite.config.js
@@ -18,14 +18,14 @@ export default defineConfig({
         orientation: 'portrait',
         scope: '/',
         start_url: '/',
-       icons: [
-        { src: 'favicon-32x32.png',    sizes: '32x32',   type: 'image/png' },
-        { src: 'icon-192.png',          sizes: '192x192', type: 'image/png' },
-        { src: 'icon-512.png',          sizes: '512x512', type: 'image/png' },
-        { src: 'icon-maskable-192.png', sizes: '192x192', type: 'image/png', purpose: 'maskable' },
-        { src: 'icon-maskable-512.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' },
-        ]
+        icons: [
+          { src: 'favicon-32x32.png',    sizes: '32x32',   type: 'image/png' },
+          { src: 'icon-192.png',          sizes: '192x192', type: 'image/png' },
+          { src: 'icon-512.png',          sizes: '512x512', type: 'image/png' },
+          { src: 'icon-maskable-192.png', sizes: '192x192', type: 'image/png', purpose: 'maskable' },
+          { src: 'icon-maskable-512.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' },
+          ]
       }
     })
   ],
-}
+})
